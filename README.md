713548f4932081e1f3f543240adee2032134a2f7
Pete Lumbis <pete@upbound.io>
2022-11-11 18:40:44 -0500

    Update theme from Geekdoc to Bootstrap based hybrid. Initial working version. Resolves #199, #198, #197, #194, #191, #190, #189, #188, #186, #184, #185, #182, #181, #122
    Signed-off-by: Pete Lumbis <pete@upbound.io>

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ For detailed information about contributing to documentation read the [Docs Cont
 
 ## License
 The Crossplane documentation is under the [Creative Commons Attribution](https://creativecommons.org/licenses/by/4.0/) license. CC-BY allows reuse, remixing and republishing of Crossplane documentation with attribution to the Crossplane organization.
+
+The docs.crossplane.io theme is based on [Geekdoc](https://github.com/thegeeklab/hugo-geekdoc) and [Bootstrap](https://github.com/twbs/bootstrap), both licensed under the MIT License.
--- utils/webpack/webpack.config.js
+++ utils/webpack/webpack.config.js
@@ -5,16 +5,16 @@ const { WebpackManifestPlugin } = require("webpack-manifest-plugin")
 const config = {
   devtool: "source-map",
   entry:{
-    main: [path.resolve("src", "js", "app.js"),
-    path.resolve("src", "js", "search.js")
-    ],
+    // main: [path.resolve("src", "js", "app.js"),
+    // path.resolve("src", "js", "search.js")
+    // ],
     colorMode: [path.resolve("src", "js", "colorMode.js")],
     lightbox: [path.resolve("src", "js", "lightbox.js")]
   },
   output: {
     filename: "[name]-[contenthash:8].bundle.min.js",
     chunkFilename: "[name]-[contenthash:8].chunk.min.js",
-    path: path.join(__dirname, "../../themes/geekdoc/assets/js"),
+    path: path.join(__dirname, "../../themes/geekboot/assets/js"),
     clean: true
   },
 
