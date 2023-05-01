bff3de632c86093ff9dd6431bc2f9cf819d8a426
Pete Lumbis <plumbis@users.noreply.github.com>
2023-05-01 10:12:39 -0400

    Search index only for /latest (#423)

---
+++ netlify_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+LATEST_VER="1.12"
+
+cp -R content/v$LATEST_VER content/latest
+
+sed -i "s/^\s*version: \"$LATEST_VER\"//g" content/latest/_index.md
+
+sed -i 's/# writeStats: true/writeStats: true/g' config.yaml
+cat config.yaml
+
+if [ $CONTEXT = "production"]
+then
+hugo --minify --baseURL $URL
+else
+hugo --minify --baseURL $DEPLOY_URL
+fi
