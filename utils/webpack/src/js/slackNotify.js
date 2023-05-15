80a1ea351d27fd743c86d2f1d80b12dc9a39839c
Pete Lumbis <pete@upbound.io>
2023-05-15 12:56:01 -0400

    Create a popper.js element for slack notification
    Signed-off-by: Pete Lumbis <pete@upbound.io>

---
+++ utils/webpack/src/js/slackNotify.js
@@ -0,0 +1,16 @@
+import { createPopper } from '@popperjs/core';
+
+const slackIcon = document.querySelector('#slack');
+const tooltip = document.querySelector('#tooltip');
+
+createPopper(slackIcon, tooltip, {
+  modifiers: [
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 12],
+      },
+    },
+  ],
+});
+
