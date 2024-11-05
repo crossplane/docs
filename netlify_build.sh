8b253bc6738fa18b206114cc12c944b7e0a83e39
Hasan Turken <turkenh@gmail.com>
2024-11-05 12:51:26 +0300

    Update documentation for v1.18 release
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Which which version is the "Latest"?
-LATEST_VER="1.17"
+LATEST_VER="1.18"
 
 # Make a copy of /content/$LATEST_VER to the directory /latest
 # Search indexing only points to /latest, this prevents broken or out of date
